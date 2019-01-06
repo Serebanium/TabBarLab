a15feaa81bba28a801a775a493f66365f2f808bc
Сергей Иванов <sergejivanov@Mac-mini-Sergej.local>
2019-01-06 21:56:45 +0300

    Initial Commit

---
+++ TabBarLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarLab
+//
+//  Created by Сергей Иванов on 06/01/2019.
+//  Copyright © 2019 topMob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
